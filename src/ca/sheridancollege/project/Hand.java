3cc432ba032832d79dd2d5a61c2db46604496b4a
blndmstf <mustafbl@sheridancollege.ca>
2023-03-27 17:27:35 -0400

    Added Hand class

---
+++ src/ca/sheridancollege/project/Hand.java
@@ -0,0 +1,11 @@
+package ca.sheridancollege.project;
+
+import ca.sheridancollege.project.GroupOfCards;
+/**
+ * Represents a player's hand
+ *
+ * @author Blend Mustafa March 2023
+ */
+public class Hand {
+
+}
