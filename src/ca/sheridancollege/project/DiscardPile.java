5e3eecb931842078144dbc6d55478117b88184df
blndmstf <mustafbl@sheridancollege.ca>
2023-03-27 17:32:58 -0400

    Added DiscardPile class

---
+++ src/ca/sheridancollege/project/DiscardPile.java
@@ -0,0 +1,8 @@
+package ca.sheridancollege.project;
+/**
+ * Discard pile, this is the pile where players discard their cards
+ *
+ * @author Blend Mustafa March 2023
+ */
+public class DiscardPile {
+}
