5d8d50693388b900e6a238411385721bc842c559
blndmstf <mustafbl@sheridancollege.ca>
2023-03-27 12:51:40 -0400

    Added description to UnoGame class

--- src/ca/sheridancollege/project/UnoGame.java
+++ src/ca/sheridancollege/project/UnoGame.java
@@ -1,6 +1,11 @@
 package ca.sheridancollege.project;
 
 public class UnoGame extends Game {
+    /**
+    * The class for the Uno game, extends from the abstract class Game  
+    * 
+    * @author Blend Mustafa March 2023
+    */
     public UnoGame(String name){
         super(name);   
     }
