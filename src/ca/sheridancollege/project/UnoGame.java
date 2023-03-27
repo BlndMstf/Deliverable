1cfa7545684ea4bd20673b246b3ba31c3d4e70b8
blndmstf <mustafbl@sheridancollege.ca>
2023-03-27 12:43:29 -0400

    Added UnoGame class

---
+++ src/ca/sheridancollege/project/UnoGame.java
@@ -0,0 +1,17 @@
+package ca.sheridancollege.project;
+
+public class UnoGame extends Game {
+    public UnoGame(String name){
+        super(name);   
+    }
+    
+    @Override
+    public void play(){
+    
+    };
+    
+    @Override
+    public void declareWinner(){
+    
+    };
+}
