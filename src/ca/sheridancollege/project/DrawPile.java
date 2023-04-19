29d32c9aa11e13da96a517e08920823f909ad9a3
blndmstf <mustafbl@sheridancollege.ca>
2023-04-19 18:15:27 -0400

    9 JUNIT tests

--- src/ca/sheridancollege/project/DrawPile.java
+++ src/ca/sheridancollege/project/DrawPile.java
@@ -26,9 +26,11 @@ public class DrawPile{
     public void fill() {
         for (UnoCard.Color color : UnoCard.Color.values()) {
             for (UnoCard.Value value : UnoCard.Value.values()) {
+                /*
                 if (value == UnoCard.Value.WILD_CARD || value == UnoCard.Value.DRAW_FOUR) {
                     continue; // Skip wildcards and draw four cards
                 }
+                */
                 UnoCard card = new UnoCard(color, value);
                 cards.add(card);
             }
