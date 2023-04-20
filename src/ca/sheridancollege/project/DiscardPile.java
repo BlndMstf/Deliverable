eea3778470b6cde9e7f81d6c477624b38605a202
sakalli02 <123204095+sakalli02@users.noreply.github.com>
2023-04-19 22:02:14 -0400

    Update DiscardPile.java

--- src/ca/sheridancollege/project/DiscardPile.java
+++ src/ca/sheridancollege/project/DiscardPile.java
@@ -1,25 +1,37 @@
-
-
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 /**
- * This class is Software Design SYST17796 18401
- *
- * @author Mehmet Sakalli
- * @author Blend Mustafa
+ * This class represents the discard pile in a game of UNO.
  */
 public class DiscardPile {
-    private UnoCard topCard;
+
+    private final ArrayList<UnoCard> cards = new ArrayList<>();
+    private UnoCard topCard; // Top card of the discard pile
 
     public DiscardPile() {
-        topCard = null;
     }
 
     public UnoCard getTopCard() {
         return topCard;
     }
 
+    public ArrayList<UnoCard> getCards() {
+        return cards;
+    }
+
     public void setTopCard(UnoCard card) {
         topCard = card;
+    }
+
+    public void addCard(UnoCard card) {
+        cards.add(card);
+        topCard = card; // Set the added card as the new top card
+    }
+
+    public void clear() {
+        cards.clear();
+        topCard = null;
     }
 }
