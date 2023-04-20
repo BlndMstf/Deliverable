236b38de6a934879f234f919c16fdc8d53931c56
sakalli02 <123204095+sakalli02@users.noreply.github.com>
2023-04-19 21:32:24 -0400

    Update UnoGame.java

--- src/ca/sheridancollege/project/UnoGame.java
+++ src/ca/sheridancollege/project/UnoGame.java
@@ -34,59 +34,55 @@ public class UnoGame extends Game {
     }
     
     public void takeTurn(UnoPlayer player){
+        Scanner scanner = new Scanner(System.in);
         PlayerActions actions = new PlayerActions(player, players, discardPile, drawPile);
-        int drawLimit = 0; //limits number of draws you can make
-        int playerHandSize = player.getHand().getCards().size(); //size of the player's hand
-        while (true){
-            //This whole code activates whenever it's a new turn
-            System.out.println("The top card is " + discardPile.getTopCard()); //Displays the top card of the discard pile
-            System.out.println(player.getName() + ", here are your cards.");
-            for (int i = 0; i < playerHandSize; i++){
-                System.out.println((i + 1) + ". " + player.getHand().getCards().get(i).toString()); //Prints each card
+        int drawLimit = 0;
+
+        do {
+            System.out.println("The top card is " + discardPile.getTopCard());
+            System.out.println(player.getName() + ", here are your cards:");
+
+            var playerHand = player.getHand().getCards();
+
+            for (int i = 0; i < playerHand.size(); i++) {
+                System.out.println((i + 1) + ". " + playerHand.get(i).toString());
             }
-            System.out.println((playerHandSize + 1) + ". Draw a card"); //There should be some logic to make this draw a card
+
+            System.out.println((playerHand.size() + 1) + ". Draw a card");
             System.out.print("Enter the index number of the card you would like to play: ");
-            int cardIndex = scanner.nextInt()-1; //ex. if the player enters 1, it should go to index 0, hence -1
-        
-            /*
-            sets the top card to be whatever the player discarded if valid,
-            then removes the card from the players hand. otherwise, prompts the player again
-            */
-            if (cardIndex >= 0 && cardIndex < playerHandSize){
-                if (actions.canPlayCard((UnoCard)player.getHand().getCard(cardIndex))==true){
-                    actions.playCard((UnoCard)player.getHand().getCard(cardIndex));
+            int cardIndex = scanner.nextInt() - 1;
+
+            if (cardIndex >= 0 && cardIndex < playerHand.size()) {
+                if (actions.canPlayCard((UnoCard)playerHand.get(cardIndex))) {
+                    actions.playCard((UnoCard)playerHand.get(cardIndex));
                     break;
-                }
-                else {
+                } else {
                     System.out.println("PLEASE CHOOSE A DIFFERENT CARD.");
                 }
-            }
-            else if (cardIndex == playerHandSize && drawLimit == 0){ //if player chooses to draw a card
+            } else if (cardIndex == playerHand.size() && drawLimit == 0) {
                 actions.playerDrawsCard();
-                playerHandSize = player.getHand().getCards().size(); //gets the new size
+                playerHand = player.getHand().getCards();
                 drawLimit++;
-            }
-            else {
+            } else {
                 System.out.println("YOU CANNOT DRAW MORE CARDS THIS ROUND");
-            
+
                 boolean canPlayCard = false;
-                ArrayList <UnoCard> playersCards = player.getHand().getCards();
-                
-                for (UnoCard c : playersCards) {
-                    if (actions.canPlayCard(c)) {
+
+                for (var card : playerHand) {
+                    if (actions.canPlayCard(card)) {
                         canPlayCard = true;
                         break;
                     }
                 }
-                
+
                 if (!canPlayCard) {
                     System.out.println("There are no cards you can play, your turn is skipped");
                     actions.skipTurn();
                 }
             }
-            
-        }
+        } while (true);
     }
+
 
     @Override
     public void play() {
